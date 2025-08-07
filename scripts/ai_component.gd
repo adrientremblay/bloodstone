82566635b45537c3f993b18eaac5c56f1f265ebc
Adrien Tremblay <adrien@adrientremblay.com>
2025-08-06 20:17:43 -0400

    Entities face player

--- scripts/ai_component.gd
+++ scripts/ai_component.gd
@@ -5,6 +5,9 @@ var rng = RandomNumberGenerator.new()
 
 # Properties
 var enabled = true
+enum AiMode {IDLE, CHASING_PLAYER}
+var mode: AiMode = AiMode.IDLE
+var player: Player = null
 
 # Exports
 @export var speed = 0.5
@@ -20,6 +23,14 @@ func _ready() -> void:
 		find_new_target()
 
 func _physics_process(delta: float) -> void:
+	if player != null:
+		navigation_agent.target_position = player.global_position
+		
+		# look at player
+		var direction = (player.global_position - body.global_position).normalized()
+		direction.y = 0  # ignore vertical difference (just rotate on Y-axis)
+		body.look_at(body.global_position + direction)
+	
 	if navigation_agent.is_target_reached() || not enabled:
 		return
 	
@@ -31,12 +42,22 @@ func find_new_target() -> void:
 	if not enabled:
 		return
 	
-	var random_angle = rng.randi_range(0, PI * 2)
-	var target_position = body.global_position + Vector3(target_radius,0,0).rotated(Vector3.UP,random_angle)
-	body.rotation = Vector3(0, random_angle, 0)
-	navigation_agent.target_position = target_position
+	if mode == AiMode.IDLE:
+		var random_angle = rng.randi_range(0, PI * 2)
+		var target_position = body.global_position + Vector3(target_radius,0,0).rotated(Vector3.UP,random_angle)
+		body.look_at(target_position)
+		navigation_agent.target_position = target_position
 	
 	model_component.play_animation("Walk")
 
 func _on_find_new_target_timer_timeout() -> void:
 	find_new_target() 
+
+func _on_player_detection_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player") and mode != AiMode.CHASING_PLAYER:
+		switch_mode(AiMode.CHASING_PLAYER)
+		player = body		
+
+func switch_mode(new_mode: AiMode):
+	mode = new_mode
+	find_new_target()
