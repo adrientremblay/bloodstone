c9541b3ebe406a2725383d0c51e09d8c215b83ee
Adrien Tremblay <adrien@adrientremblay.com>
2025-08-09 15:50:25 -0400

    Added ammo display in HUD

---
+++ scripts/hud.gd
@@ -0,0 +1,10 @@
+extends Control
+
+@onready var blood_bar = $BloodBar
+@onready var health_bar = $HealthBar
+
+func _on_player_consumed_blood(amount: Variant) -> void:
+	blood_bar.value += amount
+
+func _on_player_update_health(health: Variant) -> void:
+	health_bar.value = health
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,10 +1,1 @@
 extends Node3D
-
-@onready var blood_bar = $BloodBar
-@onready var health_bar = $HealthBar
-
-func _on_player_consumed_blood(amount: Variant) -> void:
-	blood_bar.value += amount
-
-func _on_player_update_health(health: Variant) -> void:
-	health_bar.value = health
