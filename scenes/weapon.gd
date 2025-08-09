1cb4d9e77e6f60cc559c0486809aed53f7f38312
Adrien Tremblay <adrien@adrientremblay.com>
2025-08-09 01:19:24 -0400

    Implemented vampire hand animation tree

--- scenes/weapon.gd
+++ scenes/weapon.gd
@@ -1,8 +1,18 @@
 class_name Weapon extends Node3D
 
 @onready var weapon_sound: AudioStreamPlayer = $WeaponSound
+@onready var animation_tree: AnimationTree = $AnimationTree
 
 @export var animation_name: String
 
 func fire():
 	weapon_sound.play()
+
+func switch_to_animation(animation_name: String):
+	animation_tree["parameters/conditions/attack"] = false
+	
+	animation_tree["parameters/conditions/" + animation_name] = true
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Attack":
+		animation_tree["parameters/conditions/attack"] = false
--- scripts/player.gd
+++ scripts/player.gd
@@ -24,7 +24,7 @@ signal consumed_blood(amount)
 signal update_health(health)
  
 # Properties
-#@onready var current_weapon: Weapon = hand
+@onready var current_weapon: Weapon = hand
 var blood_drain = 10 # how much blood the player is able to drain per each feed
 var health = 100
 
@@ -81,23 +81,26 @@ func _input(event: InputEvent) -> void:
 		attack()
 	elif event.is_action_pressed("previous_weapon"):
 		var other_weapon: Weapon = null
-#		if current_weapon == hand:
-#			other_weapon = pistol
-#		else:
-#			other_weapon = hand
+		if current_weapon == hand:
+			other_weapon = pistol
+		else:
+			other_weapon = hand
 		
-#		switch_weapon(other_weapon)
+		switch_weapon(other_weapon)
 
-#func switch_weapon(weapon: Weapon):
-#	current_weapon.visible = false
-#	current_weapon = weapon
-#	current_weapon.visible = true
-#	weapon_swap.play()
+func switch_weapon(weapon: Weapon):
+	current_weapon.visible = false
+	current_weapon = weapon
+	current_weapon.visible = true
+	weapon_swap.play()
 
 func attack() -> void:
 	#animate and play sound
-#	weapon_animation_player.play(current_weapon.animation_name)
-#	current_weapon.fire()
+	if current_weapon == hand:
+		current_weapon.switch_to_animation("attack")
+	else:
+		weapon_animation_player.play(current_weapon.animation_name) # legacy animation for pistol
+	current_weapon.fire()
 	
 	# Shooting Ray
 	var space = get_world_3d().direct_space_state
@@ -109,12 +112,12 @@ func attack() -> void:
 			var blood_consumed = result.collider.suffer_attack(self)
 			if blood_consumed != 0:
 				consumed_blood.emit(blood_consumed)
-#		elif current_weapon != hand: 
+		elif current_weapon != hand: 
 			# Create the bullet hole
-#			var new_bullet_hole = bullet_hole_scene.instantiate()
-#			result.collider.add_child(new_bullet_hole)
-#			new_bullet_hole.global_transform.origin = result.position
-#			new_bullet_hole.look_at(result.position + result.normal, Vector3.UP)
+			var new_bullet_hole = bullet_hole_scene.instantiate()
+			result.collider.add_child(new_bullet_hole)
+			new_bullet_hole.global_transform.origin = result.position
+			new_bullet_hole.look_at(result.position + result.normal, Vector3.UP)
 
 func take_damage(damage: int):
 	health -= damage
