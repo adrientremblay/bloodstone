d40ea0a614e44031099149280b879d7696a28c17
Adrien Tremblay <adrien@adrientremblay.com>
2025-08-11 19:27:59 -0400

    added ammo pickup sound

--- scenes/pickup.gd
+++ scenes/pickup.gd
@@ -4,4 +4,7 @@ func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
 		var p: Player = body
 		p.current_weapon.add_ammo(9)
-		self.queue_free()
+		$AudioStreamPlayer.play()
+
+func _on_audio_stream_player_finished() -> void:
+	self.queue_free()
