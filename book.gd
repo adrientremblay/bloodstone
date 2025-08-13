d6e990414b708482661368a2aef7aed5abd48c3d
Adrien Tremblay <adrien@adrientremblay.com>
2025-08-13 00:38:44 -0400

    Crappy code for the book text

--- book.gd
+++ book.gd
@@ -8,13 +8,20 @@ func get_pages() -> Array:
 	var pages = []
 	var current_page = ""
 	var words = contents.split(" ")
+	var current_page_length = 0
 	
 	for word in words:
-		if current_page.length() + word.length() > CHARACTERS_PER_PAGE:
+		var word_length = word.length()
+		if word.contains('\n'):
+			word_length += 50 * word.count('\n')
+		
+		if current_page.length() + word_length > CHARACTERS_PER_PAGE:
 			pages.append(current_page)
 			current_page = ""
+			current_page_length = 0
 			
 		current_page += " " + word
+		current_page_length += word_length + 1
 	
 	if current_page != "":
 		pages.append(current_page)
