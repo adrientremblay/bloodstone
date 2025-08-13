67d14daf36519ca828c54dade5bc874837e86f80
Adrien Tremblay <adrien@adrientremblay.com>
2025-08-12 22:20:52 -0400

    Show and hide the mouse when appropriate

--- scripts/main.gd
+++ scripts/main.gd
@@ -7,3 +7,7 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("inspect") and player.book != null:
 		hud.toggle_book(player.book.description, player.book.get_pages())
 		player.frozen = not player.frozen
+		if player.frozen:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
