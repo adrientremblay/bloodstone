feebffeab043bc309430fc96a92e66bb46726ec3
Adrien Tremblay <adrien@adrientremblay.com>
2025-08-13 21:58:53 -0400

    Interacting with the skull starts the dialogic popup

--- scripts/main.gd
+++ scripts/main.gd
@@ -5,14 +5,16 @@ extends Node3D
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("inspect"):
+		var talkable = player.return_talkable()
 		if player.book != null:
 			hud.toggle_book(player.book.description, player.book.get_pages())
+		
+		if talkable or player.book:
 			player.frozen = not player.frozen
-			if player.frozen:
-				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			else:
-				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 			
-		var talkable = player.return_talkable()
-		if talkable:
-			print("Dialog")
+		if player.frozen:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			
+			Dialogic.start("father")
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
