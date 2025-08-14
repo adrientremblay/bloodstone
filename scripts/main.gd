974ac787b21cc0ab9b56dce393b917017aee70a0
Adrien Tremblay <adrien@adrientremblay.com>
2025-08-13 22:32:26 -0400

    Player no longer stuck when dialog ends

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,18 +3,29 @@ extends Node3D
 @onready var player: Player = $Player
 @onready var hud = $HUD
 
+func _ready() -> void:
+	Dialogic.timeline_ended.connect(on_timeline_finished)
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("inspect"):
 		var talkable = player.return_talkable()
+		if not (talkable or player.book):
+			return
+			
+		player.frozen = not player.frozen
+		
 		if player.book != null:
 			hud.toggle_book(player.book.description, player.book.get_pages())
-		
-		if talkable or player.book:
-			player.frozen = not player.frozen
 			
 		if player.frozen:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			
-			Dialogic.start("father")
+			if talkable:
+				Dialogic.start("father")
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			if talkable:
+				Dialogic.end_timeline
+
+func on_timeline_finished():
+	player.frozen = false
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
