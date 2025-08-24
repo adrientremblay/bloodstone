69f1b29c2a25b4ccd76365f44dfbc16908487eec
Adrien Tremblay <adrien@adrientremblay.com>
2025-08-24 12:42:52 -0400

    Made the particles look better

--- scenes/left_hand.gd
+++ scenes/left_hand.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var particle_emitter: GPUParticles3D = $GPUParticles3D
 var active = false
 
 func _ready() -> void:
@@ -9,7 +10,10 @@ func _ready() -> void:
 func activate():
 	active = true
 	animation_player.play("Activate")
+	particle_emitter.emitting = true
 
 func release():
 	active = false
 	animation_player.play("Release")
+	particle_emitter.restart()
+	particle_emitter.emitting = false
